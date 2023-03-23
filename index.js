339bb8c2db72e99be359a993fc5bb2e9e9ee66da
Pablo Pissi <pablopissi@gmail.com>
2023-03-23 00:43:52 -0300

    html to ejs

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = require('./routes')
+const ejs = require("ejs")
+const fs = require("fs")
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
- 
+
 // Static Middleware
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
 
 routes.configure(app)
 
 app.listen(PORT, HOST, function (err) {
-    if (err) console.log(err);
-    console.log("Server listening on", HOST, PORT);
+  if (err) console.log(err);
+  console.log("Server listening on", HOST, PORT);
 });
--- routes.js
+++ routes.js
@@ -1,15 +1,18 @@
 const tasks = require('./tasks')
 
 function configure(app) {
+  const tasks = ["lock", "shutdown"]
   app.get('/', function (req, res, next) {
-    res.render('index.html');
+    const port = process.env.PORT;
+    const host = process.env.HOST;
+
+    res.render('pages/index', { host, port, tasks: tasks.map(name => ({ name, url: `http://${host}:${port}/${name}/` })) });
   })
 
-  registerTask(app, "lock")
-  registerTask(app, "shutdown")
+  tasks.forEach(name => registerTask(app, name))
 }
 
-function registerTask(app, task_name){
+function registerTask(app, task_name) {
   app.get(`/${task_name}`, function (req, res, next) {
     console.log(`${task_name} received`)
     tasks[task_name]()
